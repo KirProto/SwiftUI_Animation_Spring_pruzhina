18a2ec231577e7a33b354886a6df0f24917f1149
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 09:53:22 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Animation_Spring_pruzhina/Yan_SwiftUI_Animation_Spring_pruzhinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Animation_Spring_pruzhinaApp.swift
+//  Yan_SwiftUI_Animation_Spring_pruzhina
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Animation_Spring_pruzhinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
